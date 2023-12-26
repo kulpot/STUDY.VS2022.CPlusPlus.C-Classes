00ccb7d186aeb0382f3e3545c2998d9952fa01fd
Kulpot <kulpotwahu@gmail.com>
2023-12-27 02:13:36 +0800

    ....

--- C++ Classes/CppScratchPad.cpp
+++ C++ Classes/CppScratchPad.cpp
@@ -1,3 +1,6 @@
+#include <string>
+using std::string;
+
 // -------------------------------- C++ Classes --------------------------------
 // ref link:https://www.youtube.com/watch?v=a9R9mP3oJBY&list=PLRwVmtr-pp05LyV3bYHwrFacNSNjbUqS6
 
@@ -6,6 +9,7 @@
 class Person
 {
 public:
-
+	string firstName;
+	string lastName;
 private:
 };
