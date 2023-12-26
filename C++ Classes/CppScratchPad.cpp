5b072b48ec5edc441052609f728b3467393c7b81
Kulpot <kulpotwahu@gmail.com>
2023-12-27 02:19:46 +0800

    ...

--- C++ Classes/CppScratchPad.cpp
+++ C++ Classes/CppScratchPad.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+using std::cout;
 using std::string;
+
 
 // -------------------------------- C++ Classes --------------------------------
 // ref link:https://www.youtube.com/watch?v=a9R9mP3oJBY&list=PLRwVmtr-pp05LyV3bYHwrFacNSNjbUqS6
@@ -18,8 +20,8 @@ public:
 void main()
 {	
 	//Person person = new Person();		// C# or Java
-	Person person;		// in stack instead on the heap
+	Person person;		// "person" object is in stack instead on the heap
 	person.firstName = "Kulpot";
 	person.lastName = "King";
-	cout
+	cout << person.firstName << " " << person.lastName;
 }
