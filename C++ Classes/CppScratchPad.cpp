498ed978b3b551e3fda3b698662815192fd16748
Kulpot <kulpotwahu@gmail.com>
2023-12-27 02:12:14 +0800

    ...

--- C++ Classes/CppScratchPad.cpp
+++ C++ Classes/CppScratchPad.cpp
@@ -1,3 +1,9 @@
 // -------------------------------- C++ Classes --------------------------------
 // ref link:https://www.youtube.com/watch?v=a9R9mP3oJBY&list=PLRwVmtr-pp05LyV3bYHwrFacNSNjbUqS6
 
+// C++ Classes - 
+
+class Person
+{
+
+};
